0eb63583f693d96c28e760a1460224f798be8069
Luchintha Anjana <luchinthaa@gmail.com>
2025-05-19 19:24:33 +0530

    Multi Page Get Methods

--- index.js
+++ index.js
@@ -2,6 +2,18 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+app.get("/", (req, res)=>{
+    res.send("<h1>Home Page</>");
+});
+
+app.get("/Contact", (req, res)=>{
+    res.send("<h1>Call Me: 0765994798</>");
+});
+
+app.get("/About", (req, res)=>{
+    res.send("<h1>About: I'm Luchintha</>");
+});
+
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`)
 });
